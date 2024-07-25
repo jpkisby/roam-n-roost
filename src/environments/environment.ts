5e52d4133573c7e20c0cd8ae2ad38b4374088dee
Julie Kisby <julie.pkisby@gmail.com>
2024-07-24 21:23:21 -0400

    Fix env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from "./environment.types";
 
 export const environment: Environment = {
-    cmsSpaceId: process.env['CMS_SPACE_ID'] ?? '',
-    cmsToken: process.env['CMS_TOKEN'] ?? '',
+    cmsSpaceId: 'CMS-SPACE-ID',
+    cmsToken: 'CMS-TOKEN',
 }
